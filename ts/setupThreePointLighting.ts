06daf724a9e81ab8b30dec25de52746ed04dee60
iantomarcello <ianyys94@gmail.com>
2021-06-04 17:38:50 +0800

    fix: more accurate lighting setup

--- ts/setupThreePointLighting.ts
+++ ts/setupThreePointLighting.ts
@@ -1,5 +1,6 @@
 /**
  * A quick three point lighting setup in Three.js
+ * @author Ian Yong (iantomarcello)
  */
 
 import { Light, Scene, Vector3 } from 'three';
@@ -27,23 +28,22 @@ export function setupThreePointLighting(options : setupThreePointLightingOption)
     keyLight.position.set(-cameraFar / 2, cameraFar / 4, cameraFar / 2);
     keyLight.lookAt(new Vector3());
 
-    fillLight = new DirectionalLight(0xffffff, intensity * 0.5);
+    fillLight = new DirectionalLight(0xffffff, intensity * 0.2);
     fillLight.position.set(cameraFar / 2, cameraFar / 4, cameraFar / 2);
     fillLight.lookAt(new Vector3());
 
-    backLight = new DirectionalLight(0xffffff, intensity * 0.3);
-    backLight.position.set(cameraFar / 5, cameraFar / 5, cameraFar / 4);
+    backLight = new DirectionalLight(0xffffff, intensity * 0.2);
+    backLight.position.set(0, cameraFar / 5, -cameraFar / 2);
     backLight.lookAt(new Vector3());
-
   } else {
-    keyLight = new PointLight(0xffffff, intensity, cameraFar * 2, 2);
+    keyLight = new PointLight(0xffffff, intensity, cameraFar, 1);
     keyLight.position.set(-cameraFar / 2, cameraFar / 4, cameraFar / 2);
 
-    fillLight = new PointLight(0xffffff, intensity * 0.5, cameraFar * 2, 2);
+    fillLight = new PointLight(0xffffff, intensity * 0.5, cameraFar, 1);
     fillLight.position.set(cameraFar / 2, cameraFar / 4, cameraFar / 2);
 
-    backLight = new PointLight(0xffffff, intensity * 0.3, cameraFar * 2, 2);
-    backLight.position.set(cameraFar / 5, cameraFar / 5, cameraFar / 4);
+    backLight = new PointLight(0xffffff, intensity * 0.3, cameraFar, 1);
+    backLight.position.set(0, cameraFar / 5, -cameraFar / 2);
   }
 
   if ( scene ) {
